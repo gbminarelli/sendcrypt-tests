edd680f5aa03d498c43e819c3701c26154f4d99d
Gianlucci Badiali Minarelli <gianlucci.minarelli@gmail.com>
2018-03-30 18:46:01 -0700

    Setup basic download -> decrypt cycle on the client (for multiple JPG files).

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const app = express();
 const multer = require('multer');
-const upload = multer();
+const upload = multer({ dest: 'uploads/encrypted' });
 
-let db = [];
+// let db = [];
 
 app.post('/', upload.array('encrypted'), (req, res) => {
   console.log('Got a POST request');
   // console.log(req.body);
   // console.log(req.file);
   // console.log(req.files);
-  db.push(req.files);
-  console.log(db);
-  res.send(db);
+  // db.push(req.files);
+  // console.log(db);
+  // res.send(db);
+  res.set('Content-Type', 'text/plain');
+  res.end('File is uploaded');
 });
 
 app.get('/', (req, res) => {
@@ -21,11 +23,9 @@ app.get('/', (req, res) => {
 app.get('/index.js', (req, res) => {
   res.sendFile(__dirname + '/index.js');
 });
-app.get('/folder', (req, res) => {
-  res.send(db);
+app.get('/secret-folder', (req, res) => {
+  // res.send(db);
+  //TODO list all the files that can be downloaded.
 });
-// app.get('/formDataMulterTest.js', (req, res) => {
-//   res.sendFile(__dirname + '/formDataMulterTest.js');
-// });
 
 app.listen(1234, () => console.log('App listening on port 1234!'));
--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const myApp = () => {
   // ===
   // Encrypting and Uploading:
   // ===
-  const handleFileSelect = (e) => {
-    // Creating form:
+  const handleEncryptFiles = (e) => {
     let form = new FormData();
-    // Opening XHR:
     let xhr = new XMLHttpRequest();
     xhr.open("POST", '/', true);
     // xhr.setRequestHeader('Content-Type','multipart/form-data; boundary=AaB03x');
-    // Creating key for the selected files:
     window.crypto.subtle.generateKey(
       {
         name: "AES-GCM",
@@ -40,11 +37,10 @@ const myApp = () => {
             e.target.result
           )
           .then((encrypted) => {
-            // const ivBlob = new Blob([iv], {type:"application/octet-stream"});
-            // form.append('iv', ivBlob, 'Initialization Vector');
-            // form.append('iv', new Blob([iv.buffer], {type:"application/octet-stream"}), 'Initialization Vector');
-            form.append('encrypted', new Blob([iv.buffer], {type:"application/octet-stream"}), 'Initialization Vector');
-            form.append('encrypted', new Blob([encrypted], {type:"application/octet-stream"}), 'Encrypted File');
+            console.log(encrypted);
+            console.log(iv.buffer);
+            console.log(new Blob([iv.buffer, encrypted], {type:"application/octet-stream"}));
+            form.append('encrypted', new Blob([iv.buffer, encrypted], {type:"application/octet-stream"}));
             // Checking IV value:
             // const ivReader = new FileReader();
             // ivReader.onload = (e) => {
@@ -54,7 +50,7 @@ const myApp = () => {
             // };
             // ivReader.readAsArrayBuffer(ivBlob);
             // if (form.getAll("encrypted").length === files.length) {
-            if (form.getAll("encrypted").length === (2 * files.length)) {
+            if (form.getAll("encrypted").length === files.length) {
               // Returns encrypted values on the form:
               // console.log("\nEncrypted Files:");
               // console.log(form.getAll("encrypted"));
@@ -80,7 +76,8 @@ const myApp = () => {
         // Returns the exported key data:
         // console.log("\nKeydata:");
         // console.log(keydata);
-        document.getElementById("secure-link").innerText = `Your secure link is: ${window.location.origin}/#${keydata.k}`;
+        document.getElementById("secure-link").innerText = `${window.location.origin}/#${keydata.k}`;
+        document.getElementById("secure-link").parentElement.style.display = 'block';
       })
       .catch((err) => {
         console.error(err);
@@ -90,72 +87,167 @@ const myApp = () => {
       console.error(err);
     });
   };
-  document.getElementById('files').addEventListener('change', handleFileSelect, false);
   // ===
   // Downloading and Decrypting:
   // ===
-  if (window.location.hash) {
-    let xhr = new XMLHttpRequest(),
-    files = [];
-    xhr.open("GET", '/folder', true);
-    xhr.onload = (e) => {
-      const response = JSON.parse(xhr.response);
-      console.log(response);
-      // response is the whole database (all the folders);
-      // response[0] is a reference to the first (and in this case, only) folder.
-      // TODO: create a folder structure and reference a specific folder instead of getting the first one here.
-      for (const [index, e] of response[0].entries()) {
-        // Recovering and gouping IV and Encrypted File:
-        if (e.originalname === "Initialization Vector") {
-          files.push({
-            iv: e.buffer.data,
-            file: response[0][index+1].buffer.data
+  const handleDecryptFile = (e) => {
+    if (window.location.hash) {
+      console.log(e.target.files[0]);
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        console.log(e.target.result);
+        console.log(e.target.result.slice(0,12));
+        console.log(e.target.result.slice(12));
+        const iv = e.target.result.slice(0,12);
+        const data = e.target.result.slice(12);
+        key.then((key) => {
+          // console.log(Float64Array.from(fileObject.file));
+          // console.log(Float64Array.from(fileObject.file).buffer);
+          // const fileArrayBuffer = Uint8Array.from(fileObject.file).buffer;
+          // console.log(fileArrayBuffer);
+          window.crypto.subtle.decrypt(
+            {
+              name: "AES-GCM",
+              iv: iv
+            },
+            key,
+            data
+          )
+          .then(function(decrypted){
+            // TODO MIME types.
+            console.log('success');
+            const linkDecryptedFile = (data) => {
+              const file = new Blob([data], {type: 'image/jpeg'});
+              return window.URL.createObjectURL(file);
+            };
+            document.getElementById('download-link').href = linkDecryptedFile(decrypted);
+            document.getElementById('download-link').parentElement.style.display = 'block';
+            // console.log(new Uint8Array(decrypted));
+            // return decrypted;
+          })
+          .catch(function(err){
+            console.error(err);
           });
-        }
+        })
+        .catch((err) => {
+          console.error(err);
+        });
       }
-      console.log(files);
-    };
-    xhr.send();
-    // Returns the secret key:
-    console.log(`Secret key: ${window.location.hash.slice(1)}`);
-    // Importing key:
-    window.crypto.subtle.importKey(
-      "jwk",
-      {
+      reader.readAsArrayBuffer(e.target.files[0]);
+      // const makeFile = (fileObject) => {
+      //   const reader = new FileReader();
+      //   reader.onload = (e) => {
+      //     console.log(e.currentTarget.result);
+      //     key.then((key) => {
+      //       // console.log(Float64Array.from(fileObject.file));
+      //       // console.log(Float64Array.from(fileObject.file).buffer);
+      //       // const fileArrayBuffer = Uint8Array.from(fileObject.file).buffer;
+      //       // console.log(fileArrayBuffer);
+      //       // window.crypto.subtle.decrypt(
+      //       //   {
+      //       //     name: "AES-GCM",
+      //       //     iv: fileObject.iv
+      //       //   },
+      //       //   key,
+      //       //   fileArrayBuffer
+      //       // )
+      //       // .then(function(decrypted){
+      //       //   console.log('success');
+      //       //   // console.log(new Uint8Array(decrypted));
+      //       //   // return decrypted;
+      //       // })
+      //       // .catch(function(err){
+      //       //   console.error(err);
+      //       // });
+      //     })
+      //     .catch((err) => {
+      //       console.error(err);
+      //     });
+      //   };
+      //   // TODO: fileObject is not a blob... where does it get coerced?
+      //   // reader.readAsArrayBuffer(fileObject.file);
+      // };
+      // let xhr = new XMLHttpRequest(),
+      // files = [];
+      // xhr.open("GET", '/secret-folder', true);
+      // xhr.onload = (e) => {
+      //   const response = JSON.parse(xhr.response);
+      //   console.log('\nJSON Response');
+      //   console.log(response);
+      //   // console.log('\nRaw Response');
+      //   // console.log(xhr.response);
+      //   // response is the whole database (all the folders).
+      //   // response[0] is a reference to the first (and in this case, only) folder.
+      //   // TODO: create a folder structure and reference a specific folder instead of getting the first one here.
+      //   for (const [index, e] of response[0].entries()) {
+      //     // Recovering and gouping IV and Encrypted File:
+      //     console.log(e);
+      //     const fileObject = {
+      //       // iv: e.buffer.data,
+      //       // file: response[0][index+1].buffer.data
+      //       // file: response[0][index+1]
+      //     };
+      //     let newDiv = document.createElement('div');
+      //     newButton = document.createElement('a'),
+      //     fileName = document.createTextNode(`File ${index}`);
+      //     newButton.dataset.fileIndex = index;
+      //     newButton.appendChild(fileName);
+      //     newButton.addEventListener('click', (e) => {
+      //       // console.log(e.target.dataset.fileIndex);
+      //       e.preventDefault();
+      //       // e.target.href = makeFile(files[e.target.dataset.fileIndex]);
+      //     });
+      //     newDiv.appendChild(newButton);
+      //     document.getElementById('root').appendChild(newDiv);
+      //     // Updating files[]:
+      //     files.push(fileObject);
+      //   }
+      //   console.log(files);
+      // };
+      // xhr.send();
+      // Returns the secret key:
+      console.log(`Secret key: ${window.location.hash.slice(1)}`);
+      // Importing key:
+      const key = window.crypto.subtle.importKey(
+        "jwk",
+        {
           kty: "oct",
           k: window.location.hash.slice(1),
           alg: "A256GCM",
           ext: true,
-      },
-      {
+        },
+        {
           name: "AES-GCM",
-      },
-      false,
-      ["encrypt", "decrypt"]
-    )
-    .then((key) => {
-      // window.crypto.subtle.decrypt(
-      //   {
-      //     name: "AES-GCM",
-      //     iv: ArrayBuffer(12), //The initialization vector you used to encrypt
-      //     additionalData: ArrayBuffer, //The addtionalData you used to encrypt (if any)
-      //     tagLength: 128, //The tagLength you used to encrypt (if any)
-      //   },
-      //   key, //from generateKey or importKey above
-      //   data //ArrayBuffer of the data
-      // )
-      // .then(function(decrypted){
-      //   //returns an ArrayBuffer containing the decrypted data
-      //   console.log(new Uint8Array(decrypted));
+        },
+        false,
+        ["encrypt", "decrypt"]
+      );
+      // .then((key) => {
+      //   window.crypto.subtle.decrypt(
+      //     {
+      //       name: "AES-GCM",
+      //       iv: ArrayBuffer(12), //The initialization vector you used to encrypt
+      //       additionalData: ArrayBuffer, //The addtionalData you used to encrypt (if any)
+      //       tagLength: 128, //The tagLength you used to encrypt (if any)
+      //     },
+      //     key, //from generateKey or importKey above
+      //     data //ArrayBuffer of the data
+      //   )
+      //   .then(function(decrypted){
+      //     //returns an ArrayBuffer containing the decrypted data
+      //     console.log(new Uint8Array(decrypted));
+      //   })
+      //   .catch(function(err){
+      //     console.error(err);
+      //   });
       // })
-      // .catch(function(err){
+      // .catch((err) => {
       //   console.error(err);
       // });
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-  }
+    }
+  };
+  document.getElementById('encrypt-files').addEventListener('change', handleEncryptFiles, false);
+  document.getElementById('decrypt-file').addEventListener('change', handleDecryptFile, false);
 }
 
 window.onload = myApp;
