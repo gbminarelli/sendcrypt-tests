524220095b7916a555778dfb4b28f250b2f789ed
João Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-04-03 23:57:36 -0700

    landing - square animations

---
+++ assets/js/header-animation.js
@@ -0,0 +1,64 @@
+$(function () {
+
+    //store square positions
+    let squares = [
+        // {id: 0, top: 16, left: 11.2},
+        // {id: 1, top: 24, left: 23},
+        // {id: 2, top: 33, left: 44},
+        // {id: 3, top: 45, left: 12},
+        // {id: 4, top: 66, left: 33},
+    ];
+
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+
+    console.log(windowWidth)
+
+    let square_container = {
+        max_width: windowWidth,
+        max_height: 300
+    };
+
+
+    //generate random position squares (inside square container)
+    for (let i = 0; i < 30; i++) {
+        let newSquare = {
+            id: i,
+            top: Math.floor(Math.random() * square_container.max_height)+'px',
+            left: Math.floor(Math.random() * square_container.max_width)+'px',
+        };
+        squares.push(newSquare);
+    }
+
+    console.log(squares);
+
+
+    //create squares on image
+
+    for (let i = 0; i < squares.length; i++) {
+
+        $(".squares").append(`
+        <div class="square" data-id="${squares[i].id}" style="display: none; position:absolute; top: ${squares[i].top};left:${squares[i].left};"></div>
+        `);
+    }
+
+
+    //when all the squares are added, show them
+
+
+    let squaresInterval = setInterval(() => {
+
+        $(".square").fadeOut();
+        //pickup random square and fade in and fade out
+        let n = Math.floor(Math.random() * squares.length);
+        let animatedSquare = $(`.square[data-id=${n}]`);
+        // console.log(`Animating square ${n}`);
+        animatedSquare
+
+            .fadeToggle(500);
+
+
+    }, 2000)
+
+
+});
