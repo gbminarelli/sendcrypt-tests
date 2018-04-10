799ed381a7c9e90ccd32896c569e02b38a4443d8
João Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-04-09 22:39:49 -0700

    about us and how it works section

--- assets/js/scroll-section.js
+++ assets/js/scroll-section.js
@@ -34,10 +34,28 @@ $(function() {
         $('html, body').animate({
             scrollTop: $("#section-open-source").offset().top - 50
         }, 1000);
-
-
         changeActiveClass($(this));
     });
+
+    /* OPEN SOURCE SECTION =========================================== */
+    $(".menu-how-it-works").on('click',function(){
+
+        $('html, body').animate({
+            scrollTop: $("#section-how-it-works").offset().top - 100
+        }, 1000);
+        changeActiveClass($(this));
+    });
+
+
+    /* ABOUT US SECTION =========================================== */
+    $(".menu-about-us").on('click',function(){
+
+        $('html, body').animate({
+            scrollTop: $("#section-about-us").offset().top - 50
+        }, 1000);
+        changeActiveClass($(this));
+    });
+
 
 
 
