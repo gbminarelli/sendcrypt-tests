6f8e3fad9ec5962a2411369ebe82891062ab1dda
João Paulo Furtado Silva <joaopaulofurtado@live.com>
2018-04-15 20:06:40 -0700

    send email link 80% done

--- assets/js/modal-handler.js
+++ assets/js/modal-handler.js
@@ -46,24 +46,110 @@ $(function () {
         transitionOut: 'comingOut',
         transitionInOverlay: 'fadeIn',
         transitionOutOverlay: 'fadeOut',
-        onFullscreen: function(){},
-        onResize: function(){},
-        onOpening: function(){},
-        onOpened: function(){},
-        onClosing: function(){},
-        onClosed: function(){},
-        afterRender: function(){}
+        onFullscreen: function () {
+        },
+        onResize: function () {
+        },
+        onOpening: function () {
+        },
+        onOpened: function () {
+        },
+        onClosing: function () {
+        },
+        onClosed: function () {
+        },
+        afterRender: function () {
+        }
     });
 
 
+    /* COPY URL TO CLIPBOARD =========================================== */
+
+
+    function copyUrl() {
+        $("#copy-url").on("click", function () {
+
+            let url = $("#url");
+
+
+            //copy url content to clipboard
+            copyToClipboard(url);
+
+            //show status to user.
+            let status = $(".status");
+
+            status.text("Item copied to clipboard!");
+            status.fadeIn(500);
+
+            setTimeout(() => {
+                status.fadeOut(500)
+
+
+            }, 3000);
+
+
+        });
+    }
 
     $("#get-link-btn").on('click', function () {
         event.preventDefault();
-        // $('#modal').iziModal('setZindex', 99999);
-        // $('#modal').iziModal('open', { zindex: 99999 });
-        $('#modal').iziModal('open');
 
-    })
+        let modal = $("#modal");
+
+        let modalContent = `  <!--MODAL CONTENT-->
+
+        <p class="modal-text">The secret link to this folder is...</p>
+        <p class="modal-text-big" id="url">https://www.sendcrypt.com/url/sh39aydyaa232sSGSdhas
+
+            <a href="#" id="copy-url">
+                <i class="flaticon-copy-documents-option upload-icon-copy"></i></a>
+
+        </p>
+
+        <!--END MODAL CONTENT-->`;
+        modal.iziModal('setTitle', 'Copy your URL');
+        modal.iziModal('setSubtitle', 'Store it somewhere safe');
+        $("#modal .modal-content").html(modalContent);
+        $("#copy-url").unbind("click");//avoid multiclick event stacking
+        copyUrl(); //we should call this function here, since its a dynamic generated content
+
+        $('#modal').iziModal('open');
+    });
+
+
+    /* SHARING LINK AT EMAIL =========================================== */
+
+    $("#send-email-btn").on('click', function () {
+        event.preventDefault();
+
+        let modal = $("#modal");
+
+        let modalContent = `  <!--MODAL CONTENT-->
+
+        <p class="modal-text">Type a e-mail below and we'll send a copy of your folder's link.</p>
+         
+       
+         <label for="email" class="modal-label">
+         E-mail:
+            <input type="email" name="email" id="myEmail" class="modal-input" >
+            
+                <a href="#">
+                   <div class="btn btn-primary btn-medium" id="btn-send-email">Send E-mail</div>
+</a>
+             
+ 
+            
+            </label>
+            
+          
+         
+
+        <!--END MODAL CONTENT-->`;
+        modal.iziModal('setTitle', 'E-mail your link');
+        modal.iziModal('setSubtitle', 'To yourself or someone else');
+        $("#modal .modal-content").html(modalContent);
+        $('#modal').iziModal('open');
+    });
 
 
 });
